3922debac96bf493cd1f5bd1de4a75c3cb94d986
Mitchell-Pickup-PlzHelps <62230415+Mitchell-Pickup-PlzHelps@users.noreply.github.com>
2022-05-04 21:34:59 +1000

    bug fixes
    fixed various bugs and added some reference points to the test scene

--- Dashers 2.0/Assets/Scripts/PlayerMovement.cs
+++ Dashers 2.0/Assets/Scripts/PlayerMovement.cs
@@ -17,30 +17,33 @@ public class PlayerMovement : MonoBehaviour
     public float StartDashTimer;
     float CurrentDashTimer;
     float DashDirection;
-    bool isGrounded = false;
+    public bool isGrounded = true;
     bool isDashing;
     Rigidbody2D rb;
+    public int dashCount = 0;
+    public int maxDash = 3;
    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        gameObject.tag = "Player";
     }
 
     
     void Update()
     {
         hMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump") && isGrounded) // bools default to true in an if statement so you dont need == true here
         {
             jump = true;
+            isGrounded = false;
         }
-        if(Input.GetKeyDown(KeyCode.LeftShift) && hMove != 0)
+        if(Input.GetKeyDown(KeyCode.LeftShift) && hMove != 0 && dashCount < maxDash)
         {
             isDashing = true;
             CurrentDashTimer = StartDashTimer;
             rb.velocity = Vector2.zero;
             DashDirection = (int)hMove;
+            dashCount = dashCount + 1;
         }
         if (isDashing)
         {
@@ -65,5 +68,13 @@ public class PlayerMovement : MonoBehaviour
             hazard.Reload(gameObject);
         }
         
+    }
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            dashCount = 0;
+        }
     }
 }
